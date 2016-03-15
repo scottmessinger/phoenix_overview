04d1e2886fcbed00e4363eeae0040a2f2a9bde8b
Scott Messinger <scottmessinger@gmail.com>
2016-03-15 11:58:12 -0400

    Nest messages in documents

--- web/controllers/message_controller.ex
+++ web/controllers/message_controller.ex
@@ -6,23 +6,25 @@ defmodule Docs.MessageController do
   plug :scrub_params, "message" when action in [:create, :update]
 
   def index(conn, _params) do
-    messages = Repo.all(Message)
+    doc = conn.assigns.document
+
+    messages = Repo.all(from m in assoc(doc, :messages))
     render(conn, "index.html", messages: messages)
   end
 
-  def new(conn, _params) do
-    changeset = Message.changeset(%Message{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
   def create(conn, %{"message" => message_params}) do
-    changeset = Message.changeset(%Message{}, message_params)
+    doc = conn.assigns.document
+    changeset =
+      doc
+      |> Ecto.Model.build(:messages)
+      |> Message.changeset(message_params)
+
 
     case Repo.insert(changeset) do
       {:ok, _message} ->
         conn
         |> put_flash(:info, "Message created successfully.")
-        |> redirect(to: message_path(conn, :index))
+        |> redirect(to: document_message_path(conn, :index, conn.assigns.document))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
--- web/router.ex
+++ web/router.ex
@@ -17,8 +17,9 @@ defmodule Docs.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/documents", DocumentController
-    resources "/messages", MessageController
+    resources "/documents", DocumentController do
+      resources "/messages", MessageController
+    end
   end
 
   # Other scopes may use custom stacks.
