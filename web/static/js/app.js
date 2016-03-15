9afe885d66fda33fb4cd94dc732fecbbac52c58a
Scott Messinger <scottmessinger@gmail.com>
2016-03-15 12:22:39 -0400

    Add basic javascript

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -25,3 +25,23 @@ let socket = new Socket("/socket", {
   },
   params: {token: window.userToken}
 })
+
+socket.connect()
+socket.onOpen( () => console.log("connected!") )
+
+let App = {
+  init(){
+    let docId = $("#doc-form").data("id")
+    if(!docId){ return }
+    let authorInput = $("#document_author")
+    authorInput.val("user-" + Math.floor(Math.random() * 1000))
+    let msgContainer = $("#messages")
+    let msgInput = $("#message-input")
+
+    msgInput.on("keypress", e => { if(e.which !== 13){ return }
+      msgInput.val("")
+    })
+  }
+}
+
+App.init()
